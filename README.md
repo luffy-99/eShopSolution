73bb1322b93da727c95951bd7558564b2e65f789
DESKTOP-BCT3A7L\Admin <lehuyitmta@gmail.com>
2021-03-23 01:11:54 +0700

    design database code first and use migration to update database

---
+++ README.md
@@ -0,0 +1,8 @@
+# ASP.NET core 5.0
+## Technologies
+- ASP.net core 5.0
+- EF core >5.0
+## Install package
+- Microsoft.EntityFrameworkCore.SqlServer
+- Microsoft.EntityFrameworkCore.Design
+- Microsoft.EntityFrameworkCore.Tools
---
+++ eShopSolution.Data/Migrations/20210322180515_addSeed.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class addSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2021, 3, 23, 1, 5, 15, 253, DateTimeKind.Local).AddTicks(6832),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2021, 3, 23, 0, 51, 6, 396, DateTimeKind.Local).AddTicks(4605));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2021, 3, 23, 0, 51, 6, 396, DateTimeKind.Local).AddTicks(4605),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2021, 3, 23, 1, 5, 15, 253, DateTimeKind.Local).AddTicks(6832));
+        }
+    }
+}
